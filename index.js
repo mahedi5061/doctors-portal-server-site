e88378d2ba2ff2a0437c9fc3bb1a0811eb772691
mahedi5061 <mahedi5061@gmail.com>
2021-04-12 17:12:06 +0600

    update

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.swu9d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.taqt5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 
 const app = express()
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('doctors'));
-app.use(fileUpload());
+// app.use(express.static('doctors'));
+// app.use(fileUpload());
 
-const port = 7000;
+const port = process.env.PORT ||7000;
 
 app.get('/', (req, res) => {
     res.send("hello from db it's working working")
@@ -23,9 +23,18 @@ app.get('/', (req, res) => {
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const collection = client.db("doctorsPortal").collection("doctorsBookingInfo");
+  const appointmentCollection = client.db("doctorsPortal").collection("doctorsBookingInfo");
+  app.post('/addAppointment',(req, res)=>{
+      const appointment = req.body;
+      console.log(appointment)
+      appointmentCollection.insertOne(appointment)
+      .then((result) =>{
+        res.send(result.insertedCount)
+      })
+  })
    
 });
 
-
-app.listen(process.env.PORT || port)
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
